42228acc3d6bf8f5e40439ffe9aa071804120269
Muhammad K <akhalid.code@gmail.com>
2021-01-25 17:13:23 -0800

    add read db.json functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Node module dependencies
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Set up variables for express
 var app = express();
@@ -9,30 +10,39 @@ var PORT = process.env.PORT || 3000;
 // Handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); //README
 
 // ROUTES
-// GET index.html
-app.get("/*", function(req, res) {
-	res.sendFile(path.join(__dirname, "index.html"));
-});
-
 // GET notes.html
-app.get("/notes", function(req, res) {
-	res.sendFile(path.join(__dirname, "notes.html"));
+app.get("/notes", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // GET api/notes display all notes
-app.get("api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
 	//read db.json file and return data
+	var data = fs.readFileSync("db/db.json");
+	return res.json(JSON.parse(data));
+});
+
+// GET index.html
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 //POST api/notes save note to db.json
-app.post("api/notes", function(req,res) {
+app.post("api/notes", function (req, res) {
 	var newNote = req.body;
 	//Save note
 })
 
 //DELETE api/notes/:id deletes specific note
-app.delete("api/notes/:id", function(req, res) {
+app.delete("api/notes/:id", function (req, res) {
 	//Delete specific note
 })
+
+
+//Start server listening
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
+});
